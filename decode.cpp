7de985bff5a84d1e27f48fdf0939576081266c10
William E. Skeith <wes@cs.ccny.cuny.edu>
2015-11-06 01:54:31 -0500

    Remove old subset sum function.

--- decode.cpp
+++ decode.cpp
@@ -18,32 +18,7 @@ using std::setfill;
 
 // #define DBGPRINT 1
 
-/* compute a subset S of X whch sums to t.  Return value is the size
- * of the subset; thus a return value of 0 indicates failure. */
-size_t sssum(const vector<size_t>& X, size_t t, vector<size_t>& S)
-{
-	size_t n = X.size();
-	/* now allocate table for results. */
-	vector< vector<bool> > A(n,vector<bool>(t+1,false));
-	for (size_t v = 0; v <= t; v++) {
-		A[0][v] = (X[0] == v);
-	}
-	for (size_t i = 1; i < n; i++) {
-		for (size_t v = 0; v <= t; v++) {
-			A[i][v] = A[i-1][v] || (X[i] == v) || (v>=X[i] && A[i-1][v-X[i]]);
-		}
-	}
-	/* now reconstruct the solution. */
-	if (!A[n-1][t]) return 0;
-	size_t i = n-1;
-	while (t!=0) {
-		while (i>0 && A[i-1][t]) i--;
-		S.push_back(i);
-		t -= X[i];
-	}
-	return S.size();
-}
-
+/* compute all subsets that sum to t; return the number of such subsets */
 size_t allSSums(const vector<size_t>& X, size_t t,
 		vector< vector< vector<size_t> > >& S) {
 	/* store table of all solutions (potentially exponential) for
@@ -66,7 +41,7 @@ size_t allSSums(const vector<size_t>& X, size_t t,
 	return S[t].size();
 }
 
-int subsetSumTest2() {
+int subsetSumTest() {
 	vector<size_t> X = {1,7,4,13,8};
 	vector< vector< vector<size_t> > > S;
 	size_t t;
@@ -87,27 +62,9 @@ int subsetSumTest2() {
 	return 0;
 }
 
-int subsetSumTest() {
-	vector<size_t> X = {1,7,4,13,8};
-	vector<size_t> S;
-	size_t t;
-	while (cin >> t) {
-		S.clear();
-		size_t r = sssum(X,t,S);
-		if (r) {
-			printf("solution of size %lu:\n{ ",r);
-			for (size_t j = 0; j < r; j++) {
-				printf("%lu ",S[j]);
-			}
-			printf("}\n");
-		}
-	}
-	return 0;
-}
-
 int main(int argc, char *argv[])
 {
-	// return subsetSumTest2();
+	// return subsetSumTest();
 	/* read input */
 	size_t n; cin >> n;
 	uint32_t* b = new uint32_t[n/16];
